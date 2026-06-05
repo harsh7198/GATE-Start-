58305f3d9ed75b344b1b836b676eb7240d05f7a8
harsh7198 <harshsuradkar2221@gmail.com>
2026-06-05 20:24:55 +0530

    Area of Square

---
+++ C_Programming/square_area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+   float a, area ;
+   printf("Enter a Area Of Square : ");
+   scanf("%f",&a);
+   area = a * a ;
+   printf("Area of Square is : %.2f",area);
+
+
+    return 0;
+}
