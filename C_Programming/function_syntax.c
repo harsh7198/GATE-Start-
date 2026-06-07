56f77eb4e9a34f966ba1d712939a58d2d023215d
harsh7198 <harshsuradkar2221@gmail.com>
2026-06-07 18:46:28 +0530

    Function Syntax

---
+++ C_Programming/function_syntax.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void PrintHello(); // declaration or prototype 
+
+int main(){
+        PrintHello(); // function call 
+    return 0;
+}
+
+void PrintHello(){ // function defintion 
+    printf("Hello");
+}
