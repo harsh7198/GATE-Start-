8f6701ea322e8f18a0da6cdc6171c6f689faa9b3
harsh7198 <harshsuradkar2221@gmail.com>
2026-06-10 18:42:44 +0530

    String pointer

---
+++ C_Programming/string_pointer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+  char *str = "Hello world";
+  puts(str);
+  str = "hello";
+  puts(str);
+
+
+//   char cannotchange[] = "hello world";
+//   cannotchange = "hello";
+//   puts(cannotchange);
+    return 0;
+}
