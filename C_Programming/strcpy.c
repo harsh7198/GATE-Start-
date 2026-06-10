e7e1e51c1abcf27d9fd77b94deda1bc1e22cca16
harsh7198 <harshsuradkar2221@gmail.com>
2026-06-10 18:55:12 +0530

    strcpy function

---
+++ C_Programming/strcpy.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+  
+    char old_str[] = "old string";
+    char new_str[] = "new string";
+    strcpy(new_str, old_str); // strcpy function
+        puts(new_str);
+      
+    
+    
+    return 0; 
+}
