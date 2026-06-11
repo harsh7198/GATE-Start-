b2d8e06417f89f7f91798232762963799a38e85a
harsh7198 <harshsuradkar2221@gmail.com>
2026-06-11 19:00:32 +0530

    Pointer Structure

---
+++ C_Programming/pointer_to_structure.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+ struct student {
+    int roll; 
+    float cgpa ; 
+    char name[100];
+  };
+
+int main(){
+    struct student s1 = {16,7.9,"Harsh"};
+    struct student *ptr = &s1;
+    printf("Student roll = %d\n",(*ptr).roll);
+
+
+    return 0;
+}
