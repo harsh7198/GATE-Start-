34a318399d18c66783419639c120cd184d4cea7a
harsh7198 <harshsuradkar2221@gmail.com>
2026-06-11 18:52:41 +0530

    Array Structure

---
+++ C_Programming/Array_structure.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+struct student {
+        int roll;
+        float cgpa ; 
+        char name[100];
+    };
+int main(){
+
+    struct student ece[100];
+    ece[0].roll = 1664;
+    ece[0].cgpa = 9.2 ;
+    strcpy(ece[0].name,"Harsh");
+    printf("Name = %s\n",ece[0].name);
+
+
+    return 0;
+}
