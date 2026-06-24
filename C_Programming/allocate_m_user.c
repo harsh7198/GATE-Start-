610954253d78b5585cb4fff7eff67c8049c2d8ed
harsh7198 <harshsuradkar2221@gmail.com>
2026-06-24 08:21:17 +0530

    Allocate memory by user

---
+++ C_Programming/allocate_m_user.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    
+    int *ptr ;
+    int n ;
+    printf("Enter n : ");
+    scanf("%d", &n);
+    ptr = (int*)malloc( n * sizeof(int));
+    for(int i = 0 ; i < n ; i++){
+        printf("%d\n",ptr[i]);
+    }
+
+    
+
+    return 0;
+}
