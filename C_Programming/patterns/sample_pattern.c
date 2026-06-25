9632d2161e3943f34e898114b9f07568c7ee079e
harsh7198 <harshsuradkar2221@gmail.com>
2026-06-25 23:20:17 +0530

    sample pattern

---
+++ C_Programming/patterns/sample_pattern.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+     for(int i = 1 ; i <=5 ; i++){ // printing rows 
+        for(int j = 1 ; j <=3 ; j++){ // printing columns 
+            printf(" * ");
+        }
+        printf("\n");
+     }
+
+    return 0;
+}
