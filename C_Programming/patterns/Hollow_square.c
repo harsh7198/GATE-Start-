53454a6b7b6de1553aaa8684c2298085a6b97c76
harsh7198 <harshsuradkar2221@gmail.com>
2026-06-26 10:09:44 +0530

    Hollow pattern

---
+++ C_Programming/patterns/Hollow_square.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter n : ");
+    scanf("%d", &n);
+  
+     if(n >= 3){
+        for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            if(i == 1 || i == n || j == 1 || j == n)
+                printf(" * ");
+            else
+                printf("   ");
+        }
+        printf("\n");
+    }
+    }
+    else{
+        printf("For printing hollow pattern , number must be greater or equal 3 ");
+    }
+
+
+    return 0;
+}
