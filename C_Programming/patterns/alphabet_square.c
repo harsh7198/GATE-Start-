c949e0853f8ae97dfb780c4d8b8dbcb49cd60534
harsh7198 <harshsuradkar2221@gmail.com>
2026-06-26 08:49:31 +0530

    alphabet square pattern

---
+++ C_Programming/patterns/alphabet_square.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+      int n;
+      printf("Enter Rows :");
+      scanf("%d", &n);
+      
+     for(int i = 1 ; i <=n; i++){
+        char ch = 'A';
+        for(int j = 1 ; j <=n ; j++){
+            printf(" %c ",ch);
+            ch++;
+        }
+        printf("\n");
+     }
+     printf("Pattern printed successfully...");
+    return 0;
+}
