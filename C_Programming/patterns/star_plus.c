183a870d0be5a51c8789be677dcf095a91469724
harsh7198 <harshsuradkar2221@gmail.com>
2026-06-26 09:54:18 +0530

    plus pattern

--- C_Programming/patterns/star_plus.c
+++ C_Programming/patterns/star_plus.c
@@ -5,7 +5,8 @@ int main(){
     printf("Enter n : ");
     scanf("%d", &n);
     number = (n/2) + 1 ;
-     for(int i = 1 ; i <=n; i++){
+     if(n%2!=0){
+        for(int i = 1 ; i <=n; i++){
         
         for(int j = 1 ; j <=n ; j++){
             if(j==number || i == number){
@@ -17,6 +18,10 @@ int main(){
         }
         printf("\n");
       }
+    }
+    else{
+        printf("Even number is invalid...");
+    }
 
 
     return 0;
