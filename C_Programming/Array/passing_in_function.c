869fbba5f5224f7acbfedebe6b4a48991084d6b0
harsh7198 <harshsuradkar2221@gmail.com>
2026-06-30 21:02:40 +0530

    passing individual element to function

---
+++ C_Programming/Array/passing_in_function.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void print(int x){
+    printf("%d\n", x);
+}
+
+int main(){
+    int n ; 
+    printf("Enter size : ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0 ; i < n ; i++){
+        printf("Enter elements %d : ", i);
+        scanf("%d", &arr[i]);
+    }
+    for(int i = 0 ; i < n ; i++){
+       print(arr[i]);
+    }
+
+    return 0;
+}
