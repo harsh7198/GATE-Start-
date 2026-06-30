f7afae8cf468aa0b8ea26e4cc9182fbbb9a30d4d
harsh7198 <harshsuradkar2221@gmail.com>
2026-06-30 19:00:11 +0530

    Delete element from an array

---
+++ C_Programming/Array/delete_element.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, pos;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter index to delete (0 to %d): ", n - 1);
+    scanf("%d", &pos);
+
+    // Left Shift
+    for(int i = pos; i < n - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+
+    n--;
+
+    printf("\nArray after deletion:\n");
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
